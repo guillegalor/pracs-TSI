990512566930d5fd095d105b629c889ee55977eb
guille <guillegalor@gmail.com>
2019-03-25 18:12:50 +0100

    Creación del repo

---
+++ src/practica_busqueda/Agent.java
@@ -0,0 +1,22 @@
+package practica_busqueda;
+
+import core.game.StateObservation;
+import ontology.Types;
+import tools.ElapsedCpuTimer;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Agent extends BaseAgent{
+    private ArrayList<Types.ACTIONS> lista_acciones; // Conjunto de acciones posibles
+
+    public Agent (StateObservation stateObs, ElapsedCpuTimer elapsedTimer){
+        ArrayList<Observation>[] obstacles = stateObs.getImmovablePositions();
+
+        ArrayList<Integer> tiposObs = new ArrayList<Integer>();
+
+        for (ArrayList<Observation>; ; ) {
+
+        }
+    }
+}
