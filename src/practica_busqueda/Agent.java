6e028e1df93fd4e67c7c9dad0f50692d0be44a9f
Johanna <johanna_capote_robayna@hotmail.com>
2019-03-25 19:04:07 +0100

    funcion act

--- src/practica_busqueda/Agent.java
+++ src/practica_busqueda/Agent.java
@@ -16,7 +16,76 @@ public class Agent extends BaseAgent{
         ArrayList<Integer> tiposObs = new ArrayList<Integer>();
 
         for (ArrayList<Observation>; ; ) {
-//
+
         }
+    }
+
+    public Types.ACTIONS act (StateObservation stateObs , ElapsedCpuTimer elapsedTimer){
+      Vector2d avatar = new Vector2d(stateObs.getavatarPosition().x / fescala.x , stateObs.getavatarPosition().y / fescala.y);
+
+      if((( avatar.x != ultimaPos.x ) || (avatar.y != ultimaPos.y)) && !path.isEmpty()){
+          path.remove(0);
+      }
+
+      int nGemas = 0;
+
+      if(stateObs.getAvatarResources().isEmpty() != true){
+          nGemas = stateObs.getAvatarResources().get(6);
+      }
+
+      if(path.isEmpty()){
+
+        if(nGemas == 10){
+          Vector2d portal;
+
+          ArrayList<Observation>[] posiciones = stateObs.getPortalsPositions(stateObs.getAvatarPosition());
+
+          portal = posiciones[0].get(0).position;
+
+          portal.x = portal.x / fescala.x;
+          portal.y = portal.y / fescala.y;
+
+          path = pf.getPath(avatar, portal);
+
+        }else{
+          Vector2d gema;
+
+          ArrayList<Observation>[] posiciones = stateObs.getResourcesPositions(stateObs.getAvatarPosition());
+
+          gema = posiciones[0].get(0).position ;
+
+          gema.x = gema.x / fescala.x;
+          gema.y = gema.y / fescala.y;
+
+          path = pf.getPath(avatar, gema);
+        }
+      }
+
+      if(path != null){
+        Types.ACTIONS siguienteaccion;
+        Node siguientePos = path.get(0);
+
+        if(siguientePos.position.x != avatar.x){
+          if(siguientePos.position.x > avatar.x){
+            siguienteaccion = Types.ACTIONS.ACTION_RIGHT;
+          }else{
+            siguienteaccion = Types.ACTIONS.ACTION_LEFT;
+          }
+        }else{
+          if(siguientePos.position.y > avatar.y){
+            siguienteaccion = Types.ACTIONS.ACTION_DOWN;
+          }else{
+            siguienteaccion = Types.ACTIONS.ACTION_UP;
+          }
+        }
+
+        ultimaPos = avatar;
+
+        return siguienteaccion;
+      }else{
+        return Types.ACTIONS.ACTION_NIL;
+      }
+
+
     }
 }
